61baba7ac7f42c6c8a06a02ffaa47d49cbbd3528
Ziya Mert Karakas <50174304+zmkarakas@users.noreply.github.com>
2023-11-06 23:10:13 +0100

    Add files via upload

---
+++ index.py
@@ -0,0 +1,39 @@
+
+import requests
+import json
+
+# Load configuration from config.json
+with open('config.json', 'r') as config_file:
+    config = json.load(config_file)
+
+def describe_image(file_name):
+    with open(file_name, 'rb') as image_file:
+        headers = {
+            'Content-Type': 'application/octet-stream',
+            'Ocp-Apim-Subscription-Key': config['key1']
+        }
+        uri = config['endPoint'] + '/describe?maxCandidates=3'
+
+        response = requests.post(uri, headers=headers, data=image_file)
+        print(response.json())
+
+def analyze_image(file_name):
+    with open(file_name, 'rb') as image_file:
+        headers = {
+            'Content-Type': 'application/octet-stream',
+            'Ocp-Apim-Subscription-Key': config['key1']
+        }
+        uri = config['endPoint'] + '/analyze?visualFeatures=Tags'
+
+        response = requests.post(uri, headers=headers, data=image_file)
+        print(response.json())
+
+def read_image(file_path):
+    with open(file_path, 'rb') as image_file:
+        return image_file.read()
+
+# Get a simple description
+describe_image('./Mert.jpg')
+
+# Demo dog.jpg with visual features "Tags"
+analyze_image('./Mert.jpg')
